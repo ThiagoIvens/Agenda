fe6958131b8d981319205b782072b5a62f777efb
ThiagoIvens <49540399+ThiagoIvens@users.noreply.github.com>
2019-05-19 16:39:17 -0300

    Login and Error
    Implementação da classe Login e da Classe Error, funcionando, tendo como Usuario: "admin" e Senha: "admin".

---
+++ src/main/java/login/Login.java
@@ -0,0 +1,110 @@
+package login;
+
+import javafx.scene.control.TextField;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import thiago.agenda.Main;
+
+public class Login extends Application {
+	private Button btnLogin;
+	private TextField txtUsername;
+	private TextField txtPassword;
+	private Label lblLogin;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		AnchorPane pane = new AnchorPane();
+		pane.setPrefSize(280, 120);
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+
+
+		lblLogin = new Label(thiago.agenda.textos.lblLogin);
+		lblLogin.setLayoutX(10);
+		lblLogin.setLayoutY(10);
+
+		txtUsername = new TextField();
+		txtUsername.setLayoutX(10);
+		txtUsername.setLayoutY(31);
+		txtUsername.setMaxWidth(150);
+		txtUsername.setMinWidth(150);
+		txtUsername.setPrefWidth(150);
+		
+		txtPassword = new TextField();
+		txtPassword.setLayoutX(10);
+		txtPassword.setLayoutY(70);
+		txtPassword.setMaxWidth(150);
+		txtPassword.setMinWidth(150);
+		txtPassword.setPrefWidth(150);
+		
+		btnLogin = new Button(thiago.agenda.textos.btnLogin);
+		btnLogin.setLayoutX(167);
+		btnLogin.setLayoutY(30);
+		btnLogin.setOnMouseClicked(e -> login(stage));
+
+
+		pane.getChildren().add(lblLogin);
+		pane.getChildren().add(btnLogin);
+		pane.getChildren().add(txtUsername);
+		pane.getChildren().add(txtPassword);
+
+
+		stage.setTitle(thiago.agenda.textos.appTitle);
+		stage.setResizable(false);
+
+		stage.show();
+	}
+
+	private void login(Stage stage) {
+
+		if (txtUsername.getText().isEmpty()) {
+			try {
+				new Error(thiago.agenda.textos.ErrorUser).start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+		}
+
+		if(!txtUsername.getText().equals("admin")) {
+			try {
+				new Error(thiago.agenda.textos.ErrorUser).start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+		}
+		if (txtPassword.getText().isEmpty()) {
+			try {
+				new Error(thiago.agenda.textos.ErrorPassword).start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+		}
+		
+		if (!txtPassword.getText().equals("admin")) {
+			try {
+				new Error(thiago.agenda.textos.ErrorPassword).start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+		}
+		
+		try {
+			new Main(txtUsername.getText()).start(new Stage());
+			stage.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/thiago/agenda/textos.java
+++ src/main/java/thiago/agenda/textos.java
@@ -3,4 +3,11 @@ package thiago.agenda;
 public class textos {
 	public static final String lblMain = "Bem Vindo!";
 	public static final String appTitle = "Tela";
+	public static final String lblLogin = "Login";
+	public static final String btnLogin = "Logar";
+	public static final String ErrorUser = "Usuario Invalido";
+	public static final String ErrorPassword = "Senha Invalida";
+	public static final String btnOk = "Ok";
+	
+	
 }
