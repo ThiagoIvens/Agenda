305ebb72cf0b90234db3ca23901233d3dccf1ef7
ThiagoIvens <49540399+ThiagoIvens@users.noreply.github.com>
2019-05-19 16:00:01 -0300

    Main + jfxrt.jar
    adicionado a biblioteca jfxrt.jar ao projeto;
    adicionado os itens na classe main, tal como o metodo start para confecção do projeto final

--- src/main/java/thiago/agenda/Main.java
+++ src/main/java/thiago/agenda/Main.java
@@ -1,10 +1,36 @@
 package thiago.agenda;
 
-public class Main {
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class Main extends Application{
+	private String username;
+	
+	public Main(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		AnchorPane pane = new AnchorPane();
+		pane.setPrefSize(640, 480);
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		
+		Label lblMain = new Label(textos.lblMain + " " + username);
+		lblMain.setLayoutX(10);
+		lblMain.setLayoutY(10);
+		
+		pane.getChildren().add(lblMain);
+
+		stage.setTitle(textos.appTitle);
+		stage.setResizable(false);
+
+		stage.show();
 	}
 
 }
---
+++ src/main/java/thiago/agenda/textos.java
@@ -0,0 +1,6 @@
+package thiago.agenda;
+
+public class textos {
+	public static final String lblMain = "Bem Vindo!";
+	public static final String appTitle = "Tela";
+}
